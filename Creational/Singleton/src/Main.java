583172fdba64fc345091e98247651b440c2c75f8
savindaJ <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>
2024-05-09 09:48:53 +0530

    add singlr ton design pattern

---
+++ Creational/Singleton/src/Main.java
@@ -0,0 +1,16 @@
+import lk.ijse.singleton.Singleton;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-05-09
+ * @since : 0.1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+    }
+}
---
+++ Creational/Singleton/src/lk/ijse/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package lk.ijse.singleton;
+
+
+/**
+ * @author : savindaJ
+ * @date : 2024-05-09
+ * @since : 0.1.0
+ **/
+public class Singleton {
+    private static final Singleton singleton = new Singleton();
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return singleton; 
+    }
+}
