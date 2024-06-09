d9aaa0f8f7f6e5638bf4cc029e515254ce80415a
savindaJ <thantrige32@gmail.com>
2024-06-09 09:52:08 +0530

    creat sending reqest to other service

--- micro-service/customer-service/src/main/java/lk/ijse/customer/service/customer_service/ItemController.java
+++ micro-service/customer-service/src/main/java/lk/ijse/customer/service/customer_service/ItemController.java
@@ -1,8 +1,13 @@
 package lk.ijse.customer.service.customer_service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpRequest;
 
 /**
  * @author : savindaJ
@@ -10,11 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since : 0.1.0
  **/
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/customer")
 public class ItemController {
 
+    private final RestTemplate restTemplate;
+
     @PostMapping("/save")
     public String saveItem(){
-        return "Customer saved";
+        return restTemplate.getForObject("http://localhost:8080/api/v1/items", String.class);
     }
+
+//    @GetMapping
+//    public String getItem(){
+//        return restTemplate
+//    }
 }
--- micro-service/item-service/src/main/java/lk/ijse/item/service/item_service/ItemController.java
+++ micro-service/item-service/src/main/java/lk/ijse/item/service/item_service/ItemController.java
@@ -1,8 +1,11 @@
 package lk.ijse.item.service.item_service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author : savindaJ
@@ -10,11 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since : 0.1.0
  **/
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/items")
 public class ItemController {
 
+    private final RestTemplate restTemplate;
+
     @PostMapping("/save")
     public String saveItem(){
-        return "Item saved";
+        return restTemplate.postForObject("http://localhost:8081/api/v1/customer/save", null, String.class);
+    }
+
+    @GetMapping
+    public String getItem(){
+        return "Item Getted !";
     }
 }
